4f34268c8a207b9bf4f2d9c538e4899caab3fb46
amin <amin.haghi@hyperisland.se>
2022-11-04 22:48:50 +0100

    add files

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
+import { weatherData } from "./type.js";
 let buttonEl = document.getElementById("btn");
 let inputEl = document.getElementById("inputVal");
 let weatherDiv = document.getElementById("weather");
-let bgcolor = ["#006E90", "#F18F01", "#ADCAD6", "#99C24D", "#41BBD9"];
-let selClr = Math.floor(Math.random() * bgcolor.length) + 1;
+let searchBox = document.querySelector(".searchBox");
 let arrSrchRes = [];
 ////
-buttonEl.onclick = () => {
-  document.body.style.backgroundColor = bgcolor[selClr];
+buttonEl.onclick = async () => {
   let inputValue = inputEl.value;
-  const url =
-    "http://api.weatherapi.com/v1/forecast.json?key=78e3688d63364827a4a204236223110&q=" +
-    inputValue +
-    "&aqi=no";
-  async function getData() {
+  if (inputValue.length === 0) {
+    alert("You have to type city name or country name");
+  } else {
+    buttonEl.remove();
+    inputEl.remove();
+    const url =
+      "http://api.weatherapi.com/v1/forecast.json?key=78e3688d63364827a4a204236223110&q=" +
+      inputValue +
+      "&aqi=no";
     const res = await fetch(url);
     const data = await res.json();
     weatherDiv.innerHTML = `
-    <img width="150" height="150" alt="typeofweather" src="${data.current.condition.icon}"/>
-    <p id="nameCity"> Coutry : ${data.location.country}</p>
-    <p id="nameCity"> City : ${data.location.name}</p>
-    <p id="nameCity"> Type : ${data.current.condition.text}</p>
-    <p id="nameCity"> Humidity : ${data.current.humidity}</p>
-    <p id="nameCity">T.Celsius : ${data.current.temp_c}</p>
-    <p id="nameCity">T.Fahrenheit : ${data.current.temp_f}</p>
+      <img width="150" height="150" alt="typeofweather" src="${data.forecast.forecastday[0].day.condition.icon}"/>
+      <p id="nameCity"> Coutry : ${data.location.country}</p>
+      <p id="nameCity"> City : ${data.location.name}</p>
+      <p id="nameCity"> Type : ${data.forecast.forecastday[0].day.condition.text}</p>
+      <p id="nameCity"> Humidity : ${data.current.humidity}</p>
+      <p id="nameCity">T.Celsius : ${data.current.temp_c}</p>
+      <p id="nameCity">T.Fahrenheit : ${data.current.temp_f}</p>
     `;
-    console.log(data);
+    weatherData.forEach((element) => {
+      if (element.type === data.forecast.forecastday[0].day.condition.text) {
+        document.body.style.backgroundColor = element.color;
+        const audio = new Audio(element.audio);
+        const buttonPlay = document.createElement("button");
+        searchBox.append(buttonPlay);
+        buttonPlay.textContent = "play";
+
+        buttonPlay.onclick = () => {
+          audio.play();
+          buttonPlay.disabled = true;
+          buttonPlay.style.backgroundColor = "blue";
+          buttonStop.style.backgroundColor = "red";
+        };
+        const buttonStop = document.createElement("button");
+        searchBox.append(buttonStop);
+        buttonStop.textContent = "pause";
+        buttonStop.style.backgroundColor = "#59FFA0";
+        buttonPlay.style.backgroundColor = "red";
+        buttonStop.onclick = () => {
+          audio.pause();
+          buttonPlay.disabled = false;
+          buttonPlay.style.backgroundColor = "red";
+          buttonStop.style.backgroundColor = "blue";
+        };
+        const buttonReset = document.createElement("button");
+        searchBox.append(buttonReset);
+        buttonReset.textContent = "Search another place";
+        buttonReset.style.backgroundColor = "lightblue";
+        buttonReset.onclick = () => {
+          window.location.reload();
+        };
+      }
+    });
     const objData = {
       url: `${data.current.condition.icon}`,
       country: `${data.location.country}`,
@@ -35,10 +71,6 @@ buttonEl.onclick = () => {
       tfar: `${data.current.temp_f}`,
     };
     arrSrchRes.push(objData);
-    console.log(arrSrchRes);
+    inputEl.value = "";
   }
-
-  inputEl.value = "";
-
-  getData();
 };
