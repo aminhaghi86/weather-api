cc3564acaef4af08f420c597c3c005b7c1f26a13
Amin Haghi <90243818+aminhaghi86@users.noreply.github.com>
2023-04-08 12:04:26 +0200

    fix api key

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let searchBox = document.querySelector(".searchBox");
 let arrSrchRes = [];
 ////
 buttonEl.onclick = async () => {
-  let inputValue = inputEl.value;
-  if (inputValue.length === 0) {
-    alert("You have to type city name or country name");
-  } else {
-    buttonEl.remove();
-    inputEl.remove();
-    const url =
-      "https://api.weatherapi.com/v1/forecast.json?key=78e3688d63364827a4a204236223110&q=" +
-      inputValue +
-      "&aqi=no";
-    const res = await fetch(url);
-    const data = await res.json();
-    weatherDiv.innerHTML = `
+	let inputValue = inputEl.value;
+	if (inputValue.length === 0) {
+		alert("You have to type city name or country name");
+	} else {
+		buttonEl.remove();
+		inputEl.remove();
+		const url =
+			"https://api.weatherapi.com/v1/forecast.json?key=f494d8232e9e492da9595955230804&q=" +
+			inputValue +
+			"&aqi=no";
+		const res = await fetch(url);
+		const data = await res.json();
+		weatherDiv.innerHTML = `
       <img width="150" height="150" alt="typeofweather" src="${data.forecast.forecastday[0].day.condition.icon}"/>
       <p id="nameCity"> Coutry : ${data.location.country}</p>
       <p id="nameCity"> City : ${data.location.name}</p>
@@ -27,50 +27,50 @@ buttonEl.onclick = async () => {
       <p id="nameCity">T.Celsius : ${data.current.temp_c}</p>
       <p id="nameCity">T.Fahrenheit : ${data.current.temp_f}</p>
     `;
-    weatherData.forEach((element) => {
-      if (element.type === data.forecast.forecastday[0].day.condition.text) {
-        document.body.style.backgroundColor = element.color;
-        const audio = new Audio(element.audio);
-        const buttonPlay = document.createElement("button");
-        searchBox.append(buttonPlay);
-        buttonPlay.textContent = "play";
+		weatherData.forEach((element) => {
+			if (element.type === data.forecast.forecastday[0].day.condition.text) {
+				document.body.style.backgroundColor = element.color;
+				const audio = new Audio(element.audio);
+				const buttonPlay = document.createElement("button");
+				searchBox.append(buttonPlay);
+				buttonPlay.textContent = "play";
 
-        buttonPlay.onclick = () => {
-          audio.play();
-          buttonPlay.disabled = true;
-          buttonPlay.style.backgroundColor = "blue";
-          buttonStop.style.backgroundColor = "red";
-        };
-        const buttonStop = document.createElement("button");
-        searchBox.append(buttonStop);
-        buttonStop.textContent = "pause";
-        buttonStop.style.backgroundColor = "#59FFA0";
-        buttonPlay.style.backgroundColor = "red";
-        buttonStop.onclick = () => {
-          audio.pause();
-          buttonPlay.disabled = false;
-          buttonPlay.style.backgroundColor = "red";
-          buttonStop.style.backgroundColor = "blue";
-        };
-        const buttonReset = document.createElement("button");
-        searchBox.append(buttonReset);
-        buttonReset.textContent = "Search another place";
-        buttonReset.style.backgroundColor = "lightblue";
-        buttonReset.onclick = () => {
-          window.location.reload();
-        };
-      }
-    });
-    const objData = {
-      url: `${data.current.condition.icon}`,
-      country: `${data.location.country}`,
-      city: `${data.location.name}`,
-      type: `${data.current.condition.text}`,
-      humidity: `${data.current.humidity}`,
-      tcel: `${data.current.temp_c}`,
-      tfar: `${data.current.temp_f}`,
-    };
-    arrSrchRes.push(objData);
-    inputEl.value = "";
-  }
+				buttonPlay.onclick = () => {
+					audio.play();
+					buttonPlay.disabled = true;
+					buttonPlay.style.backgroundColor = "blue";
+					buttonStop.style.backgroundColor = "red";
+				};
+				const buttonStop = document.createElement("button");
+				searchBox.append(buttonStop);
+				buttonStop.textContent = "pause";
+				buttonStop.style.backgroundColor = "#59FFA0";
+				buttonPlay.style.backgroundColor = "red";
+				buttonStop.onclick = () => {
+					audio.pause();
+					buttonPlay.disabled = false;
+					buttonPlay.style.backgroundColor = "red";
+					buttonStop.style.backgroundColor = "blue";
+				};
+				const buttonReset = document.createElement("button");
+				searchBox.append(buttonReset);
+				buttonReset.textContent = "Search another place";
+				buttonReset.style.backgroundColor = "lightblue";
+				buttonReset.onclick = () => {
+					window.location.reload();
+				};
+			}
+		});
+		const objData = {
+			url: `${data.current.condition.icon}`,
+			country: `${data.location.country}`,
+			city: `${data.location.name}`,
+			type: `${data.current.condition.text}`,
+			humidity: `${data.current.humidity}`,
+			tcel: `${data.current.temp_c}`,
+			tfar: `${data.current.temp_f}`,
+		};
+		arrSrchRes.push(objData);
+		inputEl.value = "";
+	}
 };
